810b947212948ebb106045b7fdea94499470e83d
Abhinav Anand <abhinavanand072@gmail.com>
2026-08-05 15:01:43 +0530

    Created mock data

---
+++ data/data.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        id: 1,
+        name: 'iPhone 17',
+        category: 'smartphone',
+        price: 799,
+        inStock: true
+    },
+    {
+        id: 2,
+        name: 'iPhone 17 Pro',
+        category: 'smartphone',
+        price: 1099,
+        inStock: true
+    },
+    {
+        id: 3,
+        name: 'iPad Air',
+        category: 'tablet',
+        price: 599,
+        inStock: true
+    },
+    {
+        id: 4,
+        name: 'iPad Pro',
+        category: 'tablet',
+        price: 1199,
+        inStock: true
+    },
+    {
+        id: 5,
+        name: 'MacBook Air',
+        category: 'laptop',
+        price: 999,
+        inStock: true
+    },
+    {
+        id: 6,
+        name: 'MacBook Pro',
+        category: 'laptop',
+        price: 1799,
+        inStock: true
+    },
+]
