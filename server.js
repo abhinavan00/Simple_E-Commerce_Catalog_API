03b210aea4c7e0e601ed82d6e17f9fd3e224f16d
Abhinav Anand <abhinavanand072@gmail.com>
2026-08-05 15:10:41 +0530

    Created a mock database

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import http from 'node:http';
+import { database } from './database/database.js';
 
 const PORT = 5001
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from Server')
+    const catalog = database()
+
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(catalog))
 })
 
 server.listen(PORT, () => console.log(`Server is running on ${PORT}`))
